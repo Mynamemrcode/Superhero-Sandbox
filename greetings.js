4d38c29eb8391927793644e93baf6210a2379160
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-04-08 13:34:20 -0700

    Add files via upload

--- greetings.js
+++ greetings.js
@@ -37,3 +37,69 @@ function newimage() {
 
 }
     )}
+    window.addEventListener("keydown", your_keydown);
+
+    function your_keydown(e)
+    {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(e.shiftKey == true && keyPressed == '80')
+    {
+        block_image_width = block_image_width + 10;
+        block_image_height = block_image_height + 10;
+        document.getElementById("Current_Width").innerHTML = block_image_width;
+        document.getElementById("Current_Height").innerHTML = block_image_height;	
+    }
+    if(e.shiftKey && keyPressed == '77')
+    {
+        block_image_width = block_image_width - 10;
+        block_image_height = block_image_height - 10;
+        document.getElementById("Current_Width").innerHTML = block_image_width;
+        document.getElementById("Current_Height").innerHTML = block_image_height;
+    }
+    if(keyPressed == '70')
+	{
+		newimage(thor_face.png);
+		console.log("f");
+    }
+    if(keyPressed == '66')
+	{
+		newimage(ironman_body.png);
+		console.log("b");
+    }
+    if(keyPressed == '76')
+	{
+		newimage(spiderman_legs.png);
+		console.log("i");
+    }
+    if(keyPressed == '82')
+	{
+		newimage(hulk_right_hand.png);
+		console.log("r");
+    }
+    if(keyPressed == '72')
+	{
+		newimage(hulk_left_hand.png);
+		console.log("h");
+    }
+    if(keyPressed == '38')
+	{
+		up();
+		console.log("up");
+	}
+	if(keyPressed == '40')
+	{
+		down();
+		console.log("down");
+	}
+	if(keyPressed == '37')
+	{
+		left();
+		console.log("left");
+	}
+	if(keyPressed == '39')
+	{
+		right();
+		console.log("right");
+    }
+    }
